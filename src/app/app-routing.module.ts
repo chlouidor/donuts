698fc2488d66a845fba18d08d8f8fdbee72cb4ac
BlvckCatt <mueresnowy5456@gmail.com>
2024-08-27 21:52:18 -0400

    e

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,11 @@ const routes: Routes = [
   {
     path: 'inicio',
     loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
+  },  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   }
+
 ];
 @NgModule({
   imports: [
