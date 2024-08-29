7ebd53f1711aad0c97dfb10120396377cc65f6db
christ <ch.louidor@duocuc.cl>
2024-08-28 23:36:45 -0400

    eae

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,61 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  nombre: string = "";
+  precio: string = '';
+  dona: string = "";
 
+  listaDona: any = [
+    { 
+      id: 1 ,
+      nombre: 'Clasica',
+      precio:'2.000'
+    },
+    {
+      id: 2,
+      nombre: 'Vainilla Arcoiris',
+      precio:'5.000'
+    },
+    {
+      id: 3,
+      nombre: 'Chocolate Arcoiris',
+      precio:'3.000'
+    },
+    {
+      id: 4,
+      nombre: 'Glaseada',
+      precio:'2.000'
+    },
+    {
+      id: 5,
+      nombre: 'Super8',
+      precio:'1.200'
+    },
+    {
+      id: 6,
+      nombre: 'Boston Manjar',
+      precio:'1.000'
+    },
+    {
+      id: 7,
+      nombre: 'Rollo de Canela',
+      precio:'2.200'
+    }
+  ]
+
+  constructor( private router: Router) {}
+  irPagina(){
+    //creamos nuestra variable de contexto
+    // el nombre de nuestro contexto pude ser lo que deseamos en minuscula, aca le puse el nombe de navigacion
+    let navigationextras: NavigationExtras ={
+      //la variable state siempre se tiene que llamar asi
+      state:{
+        nom:this.listaDona.nombe,
+        pre: this.listaDona.precio,
+        dona:this.listaDona.dona
+      }
+    }
+    //Puedo crear cualquier logica de programación
+    this.router.navigate(['/comprar']);
+}
 }
