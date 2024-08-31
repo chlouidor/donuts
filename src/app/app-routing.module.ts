cd87781d267dd3a9cd874a089b9e06c4e854ae6e
christ <ch.louidor@duocuc.cl>
2024-08-31 01:59:32 -0400

    ea

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -27,7 +27,15 @@ const routes: Routes = [
   {
     path: 'confirmacion-pag',
     loadChildren: () => import('./pages/confirmacion-pag/confirmacion-pag.module').then( m => m.ConfirmacionPagPageModule)
+  },  {
+    path: 'perfil',
+    loadChildren: () => import('./pages/perfil/perfil.module').then( m => m.PerfilPageModule)
+  },
+  {
+    path: 'session',
+    loadChildren: () => import('./pages/session/session.module').then( m => m.SessionPageModule)
   }
+
 
 
 ];
